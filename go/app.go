59e99f5a7f38bd67c45b4f2dfef3e9f9370d724c
Brandon Powell <bpowell29a@gmail.com>
2014-09-06 04:51:23 -0400

    Can somewhat do graphing!

--- go/app.go
+++ go/app.go
@@ -4,11 +4,30 @@ import (
 	"net/http"
 	"time"
 	"fmt"
+	"strings"
 )
 
 type Glucose struct {
 	Date time.Time
 	Level int
+}
+
+func (g Glucose) ToArray() string {
+	const layout = "Jan 2, 2006 at 3:04pm (MST)"
+	return fmt.Sprintf("[\"%s\", %d]", g.Date.Format(layout), g.Level)
+}
+
+type GlucoseSlice []Glucose
+func (g GlucoseSlice) ToJson() string {
+	var q [] string
+	q = append(q, "[[\"Date\", \"Level\"]")
+	for _, value := range g {
+		q = append(q, ",")
+		q = append(q, value.ToArray())
+	}
+	q = append(q, "]")
+
+	return strings.Join(q, "")
 }
 
 func init() {
@@ -26,14 +45,11 @@ func chartHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func testHandler(w http.ResponseWriter, r *http.Request) {
-	var a [5] Glucose
+	a := make(GlucoseSlice, 5)
 	glucose := Glucose{time.Now(), 100}
 	a[0] = glucose
 	a[1] = glucose
 
-	const layout = "Jan 2, 2006 at 3:04pm (MST)"
-	var js = fmt.Sprintf("[[\"Date\", \"Level\"],[\"%s\",%d]]", glucose.Date.Format(layout), glucose.Level)
-
 	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(js))
+	w.Write([]byte(a.ToJson()))
 }
