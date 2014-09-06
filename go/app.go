fe654b6e6f32793763a5d5e931b7d929b0a39d28
Andrew Clissold <andrewclissold@gmail.com>
2014-09-05 23:37:19 -0400

    Get GAE up and running

---
+++ go/app.go
@@ -0,0 +1,14 @@
+package mhacks
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func init() {
+	http.HandleFunc("/", handler)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "It lives!")
+}
