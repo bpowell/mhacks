12e0f3b55f2d56a2ab36a04005ab1c29e48e7296
Andrew Clissold <andrewclissold@gmail.com>
2014-09-06 00:24:11 -0400

    iOS app base

---
+++ ios/TypeOneTwo/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TypeOneTwo
+//
+//  Created by andrew on 9/6/14.
+//  Copyright (c) 2014 TypeOneTwo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
