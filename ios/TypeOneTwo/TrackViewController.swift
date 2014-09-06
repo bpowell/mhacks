d26ef6c1749f4e361b96663f38e52e425085c0c0
Andrew Clissold <andrewclissold@gmail.com>
2014-09-06 17:46:24 -0400

    iOS: display both graphs

--- ios/TypeOneTwo/TrackViewController.swift
+++ ios/TypeOneTwo/TrackViewController.swift
@@ -10,26 +10,46 @@ import UIKit
 
 class TrackViewController: UIViewController {
 
-    @IBOutlet weak var webView: UIWebView!
+    @IBOutlet weak var insulinWebView: UIWebView!
+    @IBOutlet weak var glucoseWebView: UIWebView!
+
+    var request: NSMutableURLRequest!
 
     override func viewDidLoad() {
-        sendPost()
+        loadGraphs()
     }
 
-    func sendPost() {
+    func loadGraphs() {
+        loadInsulinGraph()
+        loadGlucoseGraph()
+    }
+
+    @IBAction func refreshButtonTapped(sender: UIButton!) {
+        loadGraphs()
+    }
+
+    func loadInsulinGraph() {
+        prepareRequestForURL("http://162.243.245.232:8081/insulinGraph")
+        insulinWebView.loadRequest(request)
+    }
+
+    func loadGlucoseGraph() {
+        prepareRequestForURL("http://162.243.245.232:8081/glucoseGraph")
+        glucoseWebView.loadRequest(request)
+    }
+
+    func prepareRequestForURL(URLString: String) {
         let token = PFUser.currentUser().sessionToken
         let post = "sessiontoken=\(token)"
         let postData = post.dataUsingEncoding(NSASCIIStringEncoding, allowLossyConversion: true)!
         let postLength = "\(postData.length)"
-        let request = NSMutableURLRequest()
+        request = NSMutableURLRequest()
 
-        request.URL = NSURL(string: "http://162.243.245.232:8081/insulinGraph")
+        request.URL = NSURL(string: URLString)
         request.HTTPMethod = "POST"
         request.setValue(postLength, forHTTPHeaderField: "Content-Length")
         request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
         request.HTTPBody = postData
-
-        webView.loadRequest(request)
     }
 
 }
