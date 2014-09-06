ef15bd074f74f2d6f0280f5e307df365233d245a
Brandon Powell <bpowell29a@gmail.com>
2014-09-06 08:45:49 -0400

    Can now parse part of results

--- go/app.go
+++ go/app.go
@@ -67,6 +67,33 @@ type User struct {
 	Username string
 }
 
+type Results struct {
+	Results json.RawMessage
+}
+
+func getstuff(user User) []byte{
+	client := &http.Client{
+	}
+
+	req, _ := http.NewRequest("GET", "https://api.parse.com/1/classes/Glucose/", nil)
+	req.Header.Add("X-Parse-Application-Id","5UjI5QS3DY6ilN8r78oZSh19lbVSH7u4RoFgRSEh")
+	req.Header.Add("X-Parse-REST-API-Key", "U90G1oAVgsLUN2ntGaDFPBIR9SWFIwtsUB8OwgGC")
+	req.Header.Add("X-Parse-Session-Token", user.SessionToken)
+	resp, _ := client.Do(req)
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	var result Results
+	err := json.Unmarshal(body, &result)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	log.Printf("%+v\n", result)
+	log.Printf("%s\n", string(result.Results))
+
+	return ([]byte(body))
+}
+
 func test2(w http.ResponseWriter, r *http.Request) {
 	client := &http.Client{
 	}
@@ -87,5 +114,5 @@ func test2(w http.ResponseWriter, r *http.Request) {
 	}
 	log.Printf("%+v", user)
 
-	w.Write([]byte(body))
+	w.Write(getstuff(user))
 }
